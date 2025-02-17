7f6c449dc8e5089b6d9d430d4e52275ef9aa6beb
willowbit <69028418+willowbit@users.noreply.github.com>
2025-02-16 20:21:30 -0500

    Update resize.py
    Improved

--- resize.py
+++ resize.py
@@ -1,3 +1,7 @@
+#!/usr/bin/env python3
+
+# THE REAL ONE IS IN UR BIN
+
 import music_tag, sys, os
 from PIL import Image
 
@@ -13,22 +17,29 @@ def resize(filename):
     f.remove_tag('artwork')
 
     img = Image.open('img.jpg')
-    newsize = (500, 500)
-    img = img.resize(newsize, resample=0, box=None)
-    img.save('img.jpg', 'JPEG')
+    if img.height > 640:
+        newsize = (500, 500)
+        img = img.resize(newsize, resample=0, box=None)
+        img.save('img.jpg', 'JPEG')
 
-    with open('img.jpg', 'rb') as img_in:
-        f["artwork"] = img_in.read()
+        with open('img.jpg', 'rb') as img_in:
+            f["artwork"] = img_in.read()
 
-    f.save()
+        f.save()
+    else: print(f"img height is {img.height}, not doing anything!")
 
 if len(sys.argv) == 1:
     print("Please specify a music directory.")
 else:
     dir = sys.argv[1]
+    n = 1
     for file in os.listdir(dir):
         filename = os.fsdecode(file)
-        if filename.endswith(".mp3"):
+        if filename.endswith(".mp3") or filename.endswith(".flac"):
+            print(f"resizing file #{n}")
             resize(filename)
+            n = n + 1
         else:
             exit
+
+print("\nall done!!")
